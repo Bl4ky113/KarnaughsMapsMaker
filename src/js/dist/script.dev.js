798de4ddb7f60a79de5d8aa6e96cbb532621654c
Bl4ky113 <martinstevenhernandezortiz@gmail.com>
2021-07-16 18:37:35 -0500

    Agregado Obj Inputs

---
+++ src/js/dist/script.dev.js
@@ -0,0 +1,22 @@
+"use strict";
+
+// Made By Bl4ky113
+var get = {
+  id: document.getElementById.bind(document),
+  "class": document.getElementsByClassName.bind(document)
+};
+var input = {
+  formula: get.id("input_formula"),
+  varNum: get.id("input_num-variables"),
+  startBtn: get.id("input_start")
+};
+
+input.formula.onchange = function (event) {
+  var obj = event.target;
+  var val = obj.value;
+  getVariables(val);
+};
+
+function getVariables(obj_val) {
+  var val_arr = [ojb_val];
+}
--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,23 @@
-console.log("hello world");
+// Made By Bl4ky113
+
+const get = {
+  id: document.getElementById.bind(document),
+  class: document.getElementsByClassName.bind(document)
+};
+
+const input = {
+  formula: get.id("input_formula"),
+  varNum: get.id("input_num-variables"),
+  startBtn: get.id("input_start")
+}
+
+input.formula.onchange = (event) => {
+  let obj = event.target;
+  let val = obj.value;
+
+  getVariables(val)
+}
+
+function getVariables (obj_val) {
+  let val_arr = ;
+}
