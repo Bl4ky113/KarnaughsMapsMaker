64775a940e4fd9d51a1d9dea861742fa34d686eb
Bl4ky113 <martinstevenhernandezortiz@gmail.com>
2021-07-17 19:03:53 -0500

    Agregado funcion de agregar Variables

--- src/js/dist/script.dev.js
+++ src/js/dist/script.dev.js
@@ -7,14 +7,32 @@ var get = {
 };
 var input = {
   formula: get.id("input_formula"),
-  varNum: get.id("input_num-variables"),
   startBtn: get.id("input_start")
 };
+var operators = ["+", "*", "-", "(", ")"];
+
+function getVariables(obj_val) {
+  var varNum = 0;
+  var formulaVar = [];
+  obj_val = obj_val.split("");
+
+  for (var i = 0; i <= obj_val.length; i += 1) {
+    if (typeof obj_val[i] === "string") {
+      if (!(operators.indexOf(obj_val[i]) >= 0)) {
+        // Skip if is an operator
+        formulaVar.push(obj_val[i]);
+        varNum += 1;
+      }
+    }
+  }
+
+  return [varNum, formulaVar];
+}
+
+input.formula.oninput = function (event) {};
 
 input.formula.onchange = function (event) {
   var obj = event.target;
   var val = obj.value;
-  getVariables(val);
+  var varInfo = getVariables(val);
 };
-
-function getVariables(obj_val) {}
--- src/js/script.js
+++ src/js/script.js
@@ -7,17 +7,41 @@ const get = {
 
 const input = {
   formula: get.id("input_formula"),
-  varNum: get.id("input_num-variables"),
   startBtn: get.id("input_start")
+}
+
+const operators = [
+  "+",
+  "*",
+  "-",
+  "(",
+  ")"
+];
+
+function getVariables (obj_val) {
+  let varNum = 0;
+  let formulaVar = [];
+
+  obj_val = obj_val.split("");
+  for (let i = 0; i <= obj_val.length; i += 1) {
+    if (typeof obj_val[i] === "string") {
+      if ( !(operators.indexOf(obj_val[i]) >= 0) ) { // Skip if is an operator
+        formulaVar.push(obj_val[i]);
+        varNum += 1;
+      }
+    }
+  }
+
+  return [varNum, formulaVar];
+}
+
+input.formula.oninput = (event) => {
+  
 }
 
 input.formula.onchange = (event) => {
   let obj = event.target;
   let val = obj.value;
 
-  getVariables(val)
-}
-
-function getVariables (obj_val) {
-  
+  var varInfo = getVariables(val);
 }
